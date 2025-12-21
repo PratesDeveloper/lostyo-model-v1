bad73396d3f272548f3af7825368ab6480263515
[dyad] <pratesdeveloper@gmail.com>
2025-12-21 19:21:15 -0300

    [dyad] Atualizando o texto do copyright no footer - wrote 1 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
             </div>
 
             <p className="text-[10px] font-bold uppercase tracking-widest text-white/10">
-              © 2025 LostyoCord
+              Copyright © 2025 Apple Inc. All rights reserved
             </p>
           </div>
         </div>
