9abab0a80986237f4404f9188b26862e445d4686
[dyad] <pratesdeveloper@gmail.com>
2025-12-21 23:14:10 -0300

    Reverted all changes back to version 384451f83e47d367d5d1511686ed42528eeafe8a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "LostyoCord",
   description: "The simple way to grow your Discord community",
   icons: {
-    icon: "https://cdn.lostyo.com/logo.png",
+    icon: "/favicon.ico",
   },
 };
 
