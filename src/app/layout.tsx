691e379f25ff6dd8b06f901c5e79a74b0a5c146b
[dyad] <pratesdeveloper@gmail.com>
2025-12-21 23:13:46 -0300

    [dyad] Updated favicon to use CDN URL - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "LostyoCord",
   description: "The simple way to grow your Discord community",
   icons: {
-    icon: "/favicon.ico",
+    icon: "https://cdn.lostyo.com/logo.png",
   },
 };
 
